e300133bab3a541878e1a40998490d7b02a418d2
Sir-JamesMuritu <jemo7158@gmail.com>
2023-07-24 08:47:59 +0300

    first commit

---
+++ Javascript/into.js
@@ -0,0 +1,6 @@
+function doSomething(){
+    document.getElementById("registration").innerHTML = "Registed";
+}
+function doSomething(){
+    document.getElementById("signing").innerHTML = "Sign-Out";
+}
